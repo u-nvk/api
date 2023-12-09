34b80adcdddf3ab89a4dee844657ffc48f3021d7
iermolaev <iermolaev@abanking.ru>
2023-12-09 16:14:03 +0500

    update

--- src/services/order/dto/index.ts
+++ src/services/order/dto/index.ts
@@ -1,4 +1,4 @@
 export * from './post-create-order/post-create-order-request.dto';
 export * from './get-order/get-order-request.dto';
 export * from './get-order/get-order-response.dto';
-export * from './get-order-histoty/get-order-history-response.dto';
+export * from './get-order-history/get-order-history-response.dto';
